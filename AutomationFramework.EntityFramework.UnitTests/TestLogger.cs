52cc9ac2c7eace0079279427e5fd2bb8666439dd
Ryan Fitzsimmons <ryan.fitzsimmons@outlook.com>
2020-12-30 02:22:25 +0000

    Updated automationframework to 1.0.13

--- AutomationFramework.EntityFramework.UnitTests/TestLogger.cs
+++ AutomationFramework.EntityFramework.UnitTests/TestLogger.cs
@@ -7,91 +7,37 @@ namespace AutomationFramework.EntityFramework.UnitTests
 {
     public class TestLogger : ILogger
     {
-        public void Error(string message)
+        public void Write(LogLevels level, object message)
         {
-            Error(StagePath.Empty, message);
+            Write(level, StagePath.Empty, message);
         }
 
-        public void Error(StagePath path, string message)
+        public void Write(LogLevels level, StagePath path, object message)
         {
-            Log.Error(GetText(path, message));
+            switch (level)
+            {
+                case LogLevels.Error:
+                    Log.Error(GetText(path, message));
+                    break;
+                case LogLevels.Fatal:
+                    Log.Fatal(GetText(path, message));
+                    break;
+                case LogLevels.Information:
+                    Log.Information(GetText(path, message));
+                    break;
+                case LogLevels.Warning:
+                    Log.Warning(GetText(path, message));
+                    break;
+                default:
+                    throw new Exception($"Unknown LogLevel {level}");
+            }
         }
 
-        public void Fatal(string message)
-        {
-            Fatal(StagePath.Empty, message);
-        }
-
-        public void Fatal(StagePath path, string message)
-        {
-            Log.Fatal(GetText(path, message));
-        }
-
-        public void Information(string message)
-        {
-            Information(StagePath.Empty, message);
-        }
-
-        public void Information(StagePath path, string message)
-        {
-            Log.Information(GetText(path, message));
-        }
-
-        public void Warning(string message)
-        {
-            Warning(StagePath.Empty, message);
-        }
-
-        public void Warning(StagePath path, string message)
-        {
-            Log.Warning(GetText(path, message));
-        }
-
-        private static string GetText(StagePath path, string message)
+        private static string GetText(StagePath path, object message)
         {
             if (path == StagePath.Empty)
-                return message;
+                return message.ToString();
             return $"{path} - {message}";
-        }
-
-        public void Information(object message)
-        {
-            Information(message.ToString());
-        }
-
-        public void Information(StagePath path, object message)
-        {
-            Information(path, message.ToString());
-        }
-
-        public void Warning(object message)
-        {
-            Warning(message.ToString());
-        }
-
-        public void Warning(StagePath path, object message)
-        {
-            Warning(path, message.ToString());
-        }
-
-        public void Error(object message)
-        {
-            Error(message.ToString());
-        }
-
-        public void Error(StagePath path, object message)
-        {
-            Error(path, message.ToString());
-        }
-
-        public void Fatal(object message)
-        {
-            Fatal(message.ToString());
-        }
-
-        public void Fatal(StagePath path, object message)
-        {
-            Fatal(path, message.ToString());
         }
     }
 }
